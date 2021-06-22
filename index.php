25b20b87f692b7f87d727fc1ba84dec922bb5746
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-06-22 15:48:03 +0200

    Inizio Snack 3

--- index.php
+++ index.php
@@ -48,6 +48,52 @@ if (strlen($name) > 3 && strpos($mail, ".") && strpos($mail, "@") && is_numeric(
     $esito_2 = "Accesso negato";
 }
 
+/* Snack 3
+Creare un array di array. Ogni array figlio avrà come chiave una data in questo formato: DD-MM-YYYY es 01-01-2007 
+e come valore un array di post associati a quella data. 
+Stampare ogni data con i relativi post.
+Qui l’array di esempio: https://www.codepile.net/pile/R2K5d68z */
+
+$posts = [
+
+    '10/01/2019' => [
+        [
+            'title' => 'Post 1',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 1'
+        ],
+        [
+            'title' => 'Post 2',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 2'
+        ],
+    ],
+    '10/02/2019' => [
+        [
+            'title' => 'Post 3',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 3'
+        ]
+    ],
+    '15/05/2019' => [
+        [
+            'title' => 'Post 4',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 4'
+        ],
+        [
+            'title' => 'Post 5',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 5'
+        ],
+        [
+            'title' => 'Post 6',
+            'author' => 'Michele Papagni',
+            'text' => 'Testo post 6'
+        ]
+    ],
+];
+
 ?>
 
 <!DOCTYPE html>
@@ -75,6 +121,17 @@ if (strlen($name) > 3 && strpos($mail, ".") && strpos($mail, "@") && is_numeric(
     <p>
         <?php echo $esito_2; ?>
     </p>
+
+    <h2>Snack 3</h2>
+
+    <?php for ($i = 0; $i < count($posts); $i++) {
+    ?>
+        <h4>
+            <?php echo $posts[$i];
+            ?>
+
+        </h4>
+    <?php } ?>
 
 </body>
 
