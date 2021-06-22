bc4f1694ba5d088497e86976be92aa661129326c
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-06-22 14:56:31 +0200

    Inizio Snack 2

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
-<!-- Snack 1
+<?php
+
+/* Snack 1
 Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. 
 Ogni array avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. 
 Stampiamo a schermo tutte le partite con questo schema.
-Olimpia Milano - Cantù | 55-60 -->
-
-<?php
+Olimpia Milano - Cantù | 55-60 */
 
 $partite = [
     [
@@ -33,6 +33,15 @@ $partite = [
     ],
 ];
 
+/* Snack 2
+Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) 
+che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero.
+Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” */
+
+$name = $_GET("name");
+$mail = $_GET("mail");
+$age = $_GET("age");
+
 ?>
 
 <!DOCTYPE html>
@@ -42,7 +51,7 @@ $partite = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Snacks</title>
 </head>
 
 <body>
@@ -54,6 +63,7 @@ $partite = [
             <?php echo $partite[$i]["squadra_casa"]; ?> - <?php echo $partite[$i]["squadra_ospite"]; ?> | <?php echo $partite[$i]["punti_casa"]; ?>-<?php echo $partite[$i]["punti_ospite"]; ?>
         </p>
     <?php } ?>
+
 </body>
 
 </html>
