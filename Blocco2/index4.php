79797b5487b83af8bddb93eea6cf127c72075240
paoloferrari97 <ferrari.paolo.97@gmail.com>
2021-06-23 13:01:52 +0200

    Svolgo Snack 3A

---
+++ Blocco2/index4.php
@@ -0,0 +1,75 @@
+<!-- Snack 3 A
+Partiamo da questo array di hotel. https://www.codepile.net/pile/OEWY7Q1G
+Stampare tutti i nostri hotel con tutti i dati disponibili.
+Avremo un file PHP con il nostro “database” e un file con tutta la logica. -->
+
+<!-- Snack 3B
+Attraverso un parametro GET da inserire direttamente nell’url (uno alla volta), 
+filtrare gli hotel che hanno un parcheggio, numero minimo di stelle o massima lontananza dal centro.
+Se non c’è un filtro, visualizzare come in precedenza tutti gli hotel -->
+
+<?php
+
+$hotels = [
+
+    [
+        'name' => 'Hotel Belvedere',
+        'description' => 'Hotel Belvedere Descrizione',
+        'parking' => true,
+        'vote' => 4,
+        'distance_to_center' => 10.4
+    ],
+    [
+        'name' => 'Hotel Futuro',
+        'description' => 'Hotel Futuro Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 2
+    ],
+    [
+        'name' => 'Hotel Rivamare',
+        'description' => 'Hotel Rivamare Descrizione',
+        'parking' => false,
+        'vote' => 1,
+        'distance_to_center' => 1
+    ],
+    [
+        'name' => 'Hotel Bellavista',
+        'description' => 'Hotel Bellavista Descrizione',
+        'parking' => false,
+        'vote' => 5,
+        'distance_to_center' => 5.5
+    ],
+    [
+        'name' => 'Hotel Milano',
+        'description' => 'Hotel Milano Descrizione',
+        'parking' => true,
+        'vote' => 2,
+        'distance_to_center' => 50
+    ],
+
+];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Snacks 3</title>
+</head>
+
+<body>
+
+    <?php foreach ($hotels as $hotel) { ?>
+        <?php foreach ($hotel as $key => $value) { ?>
+            <p><?php echo $key; ?>: <?php echo $value; ?></p>
+        <?php } ?>
+    <?php } ?>
+
+</body>
+
+</html>
